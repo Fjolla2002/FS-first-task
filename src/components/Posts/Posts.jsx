9f566bf0f39854b18fed909e469c762e5d7fc395
Fjolla2002 <fjolla.selimi17@gmail.com>
2024-02-01 16:03:06 +0100

    Added styling for posts

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect }from 'react';
 import axios from 'axios';
 import './posts.scss';
+import {NavLink} from 'react-router-dom';
 
 const Posts = () => {
     const [posts, setPosts] = useState();
     const [comments, setComments] = useState();
+    const [users, setUsers] = useState();
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -14,7 +16,8 @@ const Posts = () => {
         })
         .catch((error) => {
             console.log("error", error)
-        })
+        });
+
         axios.get('https://jsonplaceholder.typicode.com/comments')
         .then((res)=>{
             setComments(res.data)
@@ -22,28 +25,49 @@ const Posts = () => {
         })
         .catch((error) => {
             console.log("error", error)
+        });
+
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((res)=>{
+            setUsers(res.data)
+            console.log(res.data);
         })
+        .catch((error) => {
+            console.log("error", error)
+        });
     }, [])
 
 
   return (
-    <div>
-        <h2>Recent Posts</h2>
+    <div className='container'>
+        <h2 className='title'>Recent Posts</h2>
         <div className='all-posts'>
-            {posts?.slice(0, 10).map((post) => (
-                <div key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p>{post.body}</p>
-
-                    <div>
+            {posts?.slice(5, 15).map((post) => (
+                <div className='single-post' key={post.id}>
+                    <div className='post-owner'>
+                        {users?.filter((user) => user.id === post.userId).map((user) => (
+                            <span key={user.id}>By <span className='username'>{user.name}</span></span>
+                        ))}
+                    </div>
+                    <div className='post-details'>
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                    </div>
+                    <div className='post-link'>
+                        <NavLink>
+                            Read More
+                        </NavLink>
+                    </div>
+                    {/* <div className='comments'>
+                        <h5>Comments</h5>
                         {comments?.filter((comment) => comment.postId === post.id).map((postComment) => (
-                            <div key={postComment.id}>
+                            <div className='single-comment' key={postComment.id}>
                                 <span>{postComment.name}</span>
                                 <span>{postComment.email}</span>
                                 <p>{postComment.body}</p>
                             </div>
                         ))}
-                    </div>
+                    </div> */}
                 </div>
             ))}
         </div>
